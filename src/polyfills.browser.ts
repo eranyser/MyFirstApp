2443f5db45aead58b6ec5c2e55a9a6be20430a22
Nadav Sinai <ns@nadavsinai.com>
2017-06-27 17:42:07 +0300

    remove long-stack-trace-zone for IE

--- src/polyfills.browser.ts
+++ src/polyfills.browser.ts
@@ -1,10 +1,14 @@
-require("@algotec/browser-helpers/polyfills");
+import "@algotec/browser-helpers/polyfills";
+import {BrowserHelpers} from "@algotec/browser-helpers";
 import "zone.js/dist/zone";
 
+const browser = BrowserHelpers.detectBrowser();
 if ('production' === ENV) {
 	// Production
 } else {
-	// Development
-	Error.stackTraceLimit = Infinity;
-	require('zone.js/dist/long-stack-trace-zone');
+	if (browser.browserName !== BrowserHelpers.browserType.IE) {
+		// Development
+		Error.stackTraceLimit = Infinity;
+		require('zone.js/dist/long-stack-trace-zone');
+	}
 }
